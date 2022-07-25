c33d305523a460d432909caf63e845bf0ae97312
FelipeKennedydaSilva <felipekennedy79@gmail.com>
2022-07-25 08:45:49 -0300

    Exercício 40a

---
+++ Ex40a.py
@@ -0,0 +1,12 @@
+nota_1 = float(input('Qual valor da primeira nota? '))
+nota_2 = float(input('Qual valor da segunda nota? '))
+media = (nota_1 + nota_2) / 2
+
+print('Resultado', end=" ")
+
+if media >= 7 and media > 10:
+    print('Aprovado')
+elif media == 10:
+    print('Aprovado com Distinção')
+else:
+    print("Reprovado")
