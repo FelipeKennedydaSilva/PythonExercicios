db107931be3f0a60961346cb2172c2a09490158c
FelipeKennedydaSilva <felipekennedy79@gmail.com>
2022-08-02 20:52:21 -0300

    Exercício 50

---
+++ Ex050.py
@@ -0,0 +1,9 @@
+# ler seis numeros e mostre a soma do numeros pares
+soma = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input(f'Digite o {c} valor: '))
+    if n % 2 == 0:
+        soma += n
+        cont += 1
+print(f'Você informou {cont} números pares e sua soma foi {soma}.')
